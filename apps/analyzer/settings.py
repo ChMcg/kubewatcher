93e1a545a110d04099e081d4103d980510b72e5f
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-05-30 21:52:38 +0300

    [apps] added analyzer app settings file

---
+++ apps/analyzer/settings.py
@@ -0,0 +1,25 @@
+import os
+
+from models import DBConnectionModel
+
+
+# selector_file = os.getenv('SELECTOR_FILE') or 'selector.json'
+# selector_file = os.getenv('SELECTOR_FILE') or 'configs/selector.json'
+selector_file = os.getenv('SELECTOR_FILE') or '../../configs/selector.json'
+
+# DBConnection = DBConnectionModel({
+#     "user":     os.getenv('POSTGRES_USER') or "inelos",
+#     "password": os.getenv('POSTGRES_PASS') or "r545TSvM",
+#     "host":     os.getenv('POSTGRES_HOST') or "postgres",
+#     "port":     os.getenv('POSTGRES_PORT') or "5432"
+# })
+
+DBConnection = DBConnectionModel({
+    "user":     os.getenv('POSTGRES_USER') or "postgres",
+    "password": os.getenv('POSTGRES_PASS') or "r545TSvM",
+    "host":     os.getenv('POSTGRES_HOST') or "localhost",
+    "port":     os.getenv('POSTGRES_PORT') or "5432"
+})
+
+database_schema_file_path = os.getenv('DATABAS_SCHEMA_PATH') or 'schema.sql'
+
