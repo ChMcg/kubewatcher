0ea5eeeb41c8a91fef528cb469a252b83fd556ea
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-05-30 21:53:36 +0300

    [apps] exceptions added

---
+++ apps/analyzer/exceptions.py
@@ -0,0 +1,9 @@
+
+class SeverityNotConfiguredExceprion(Exception):
+    pass
+
+class SubSelectorPathMissedException(Exception):
+    pass
+
+class SelectorInnerMatchFailedException(Exception):
+    pass
