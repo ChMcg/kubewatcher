5e6e4721792869b580b1dadc39281397048ff49e
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-05-31 11:10:36 +0300

    [apps] read_api implemented

--- apps/analyzer/main.py
+++ apps/analyzer/main.py
@@ -2,10 +2,12 @@
 from fastapi import FastAPI
 
 from receiver import router as receiver_router
+from read_api import router as read_api_router
 
 
 app = FastAPI()
 app.include_router(receiver_router)
+app.include_router(read_api_router)
 
 
 
