806a90639b979ff68b13cf75852a4b6e1c95f0de
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-05-31 23:00:28 +0300

    [apps] exporter fixes

--- apps/exporter/exporter.py
+++ apps/exporter/exporter.py
@@ -2,15 +2,16 @@ import time
 import requests
 import json
 import os
+
 from typing import Iterator
 
 
-# log_filename = 'logs/audit-test.log'
+
 log_filename = '/var/log/kubernetes/apiserver/audit.log'
-export_api_path = os.getenv("EXPORT_PATH") or "http://analyzer/receiver/audit"
+export_api_path = os.getenv("EXPORT_PATH") or f"http://analyzer:8000/receiver/audit"
 
 
-def follow(file, sleep_sec=0.1) -> Iterator[str]:
+def follow(file, sleep_sec=1) -> Iterator[str]:
     """ Yield each line from a file as they are written.
     `sleep_sec` is the time to sleep after empty reads. """
     line = ''
@@ -29,6 +30,9 @@ def parse_log_entry(entry: str) -> dict:
     return json.loads(entry.strip())
 
 def report_log_entry(entry: dict):
+    if 'auditID' in entry:
+        audit_id = entry['auditID']
+        print(f"Exporting audit_item with id '{audit_id}'")
     requests.put(
             export_api_path, 
             data=json.dumps(entry, ensure_ascii=False)
@@ -36,17 +40,18 @@ def report_log_entry(entry: dict):
 
 
 def main():
+    print(f"Exporter started")
+    print(f"Watching audit_log file at '{log_filename}'")
     idle = True
     while idle:
         try:
             with open(log_filename, 'r') as file:
                 for line in follow(file):
-                    print(line, end='')
                     parsed = parse_log_entry(line)
                     report_log_entry(parsed)
         except Exception as e:
             print(f"Exception occured: {e}")
-            continue
+            idle = False
 
 
 if __name__=="__main__":
