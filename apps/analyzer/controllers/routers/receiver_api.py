c88e233f832d95ffd4be8c1573e8931dffd56b6b
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-12-25 19:31:46 +0300

    [apps] get use of new ORM types in receiver router

--- apps/analyzer/controllers/routers/receiver_api.py
+++ apps/analyzer/controllers/routers/receiver_api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 
-from controllers.database import DB
 from controllers.analyzer import Analyzer
+from database.postgres import Postgres
+from models.orm import AnalyzedData, AnalyzedMetadata
 
 router = APIRouter(
         prefix='/receiver',
@@ -9,32 +10,22 @@ router = APIRouter(
         dependencies=[]
     )
 
-db = DB()
+postgres = Postgres()
 
 
 @router.put("/audit")
 def receive_audit(data: dict) -> dict:
     analyzed_object = Analyzer.analyze(data)
+
     # store result in database 
-    with db.get_cursor() as cursor:
-        cursor.execute(
-                f"insert into analyzed_data"
-                f" (object)"
-                f" values ('{analyzed_object.dump_string()}')"
-                f" returning analyzed_data_id"
-            )
-        inserted_id = cursor.fetchone()[0]
-        if 'auditID' in data:
-            audit_id = data['auditID']
-            print(f"AnalyzedObject with id {inserted_id} and auditID '{audit_id}' inserted")
-        else:
-            print(f"AnalyzedObject with id {inserted_id} inserted")
-        cursor.execute(
-                f"insert into analyzed_metadata"
-                f" (analyzed_data_id, severity, score)"
-                f" values ('{inserted_id}', '{analyzed_object.severity}', '{analyzed_object.severity_score}')"
-            )
-        db.connection.commit()
+    with postgres.session() as session:
+        analyzed_data = AnalyzedData(analyzed_object)
+        session.add(analyzed_data)
+        session.refresh(analyzed_data)
+        analyzed_metadata = AnalyzedMetadata(analyzed_object, analyzed_data)
+        session.add(analyzed_metadata)
+        session.commit()
+
     return {
         'severity': analyzed_object.severity, 
         'score': analyzed_object.severity_score
