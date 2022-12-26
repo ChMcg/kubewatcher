560e27e30a08d8872563d6263ee5eb499b1b1b18
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-12-26 09:00:32 +0300

    [k8s] cluster connect saved

--- k8s/scripts/cluster-connect.sh
+++ k8s/scripts/cluster-connect.sh
@@ -8,3 +8,6 @@ kubeadm join \
 	sha256:e04522c9a0e7e52d772e0da871e7854e1c915461ee2a970869f4551885b65428 \
 	| tee kubeadm_join.log \
 	| ccze -A
+
+kubeadm join 195.133.201.192:6443 --token ld318g.56nw6n6jtznhe46r \
+	--discovery-token-ca-cert-hash sha256:b2eda373a2ed98d146bc6e5c2d7ede21210abfebfadffd7023cc36fd759200d7
