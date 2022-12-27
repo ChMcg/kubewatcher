bd98660936568f4b64598e9d79aececdaa3c6257
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-12-27 04:18:41 +0300

    [apps] fix analyzer errors

--- apps/analyzer/controllers/routers/read_api.py
+++ apps/analyzer/controllers/routers/read_api.py
@@ -1,7 +1,7 @@
-import json
+# import json
 from fastapi import APIRouter
 
-from controllers.database import DB
+# from controllers.database import DB
 
 router = APIRouter(
         prefix='/read',
@@ -9,48 +9,48 @@ router = APIRouter(
         dependencies=[]
     )
 
-db = DB()
+# db = DB()
 
 
-@router.get("/get_data_by_severity")
-def get_data_by_severity(severity: str) -> list[dict]:
-    data = []
-    with db.get_cursor() as cursor:
-        fields = ', '.join([
-            "am.severity",
-            "am.score",
-            "ad.object",
-        ])
-        cursor.execute(
-                f"select {fields} from analyzed_data ad"
-                f" inner join analyzed_metadata am "
-                f" on ad.analyzed_data_id = am.analyzed_data_id"
-                f" where am.severity = '{severity}'"
-            )
-        ret = cursor.fetchall()
-        for fetched_row in ret:
-            _, _, fetched_object = fetched_row
-            data.append(json.loads(fetched_object))
-    return data
+# @router.get("/get_data_by_severity")
+# def get_data_by_severity(severity: str) -> list[dict]:
+#     data = []
+#     with db.get_cursor() as cursor:
+#         fields = ', '.join([
+#             "am.severity",
+#             "am.score",
+#             "ad.object",
+#         ])
+#         cursor.execute(
+#                 f"select {fields} from analyzed_data ad"
+#                 f" inner join analyzed_metadata am "
+#                 f" on ad.analyzed_data_id = am.analyzed_data_id"
+#                 f" where am.severity = '{severity}'"
+#             )
+#         ret = cursor.fetchall()
+#         for fetched_row in ret:
+#             _, _, fetched_object = fetched_row
+#             data.append(json.loads(fetched_object))
+#     return data
 
 
-@router.get("/get_data_by_score_level")
-def get_data_by_score_level(treshold: int) -> list[dict]:
-    data = []
-    with db.get_cursor() as cursor:
-        fields = ', '.join([
-            "am.severity",
-            "am.score",
-            "ad.object",
-        ])
-        cursor.execute(
-                f"select {fields} from analyzed_data ad"
-                f" inner join analyzed_metadata am "
-                f" on ad.analyzed_data_id = am.analyzed_data_id"
-                f" where am.score > {treshold}"
-            )
-        ret = cursor.fetchall()
-        for fetched_row in ret:
-            _, _, fetched_object = fetched_row
-            data.append(json.loads(fetched_object))
-    return data
+# @router.get("/get_data_by_score_level")
+# def get_data_by_score_level(treshold: int) -> list[dict]:
+#     data = []
+#     with db.get_cursor() as cursor:
+#         fields = ', '.join([
+#             "am.severity",
+#             "am.score",
+#             "ad.object",
+#         ])
+#         cursor.execute(
+#                 f"select {fields} from analyzed_data ad"
+#                 f" inner join analyzed_metadata am "
+#                 f" on ad.analyzed_data_id = am.analyzed_data_id"
+#                 f" where am.score > {treshold}"
+#             )
+#         ret = cursor.fetchall()
+#         for fetched_row in ret:
+#             _, _, fetched_object = fetched_row
+#             data.append(json.loads(fetched_object))
+#     return data
--- apps/analyzer/controllers/routers/receiver_api.py
+++ apps/analyzer/controllers/routers/receiver_api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+from sqlalchemy.exc import SQLAlchemyError
 
 from controllers.analyzer import Analyzer
 from database.postgres import Postgres
@@ -19,12 +20,20 @@ def receive_audit(data: dict) -> dict:
 
     # store result in database 
     with postgres.session() as session:
-        analyzed_data = AnalyzedData(analyzed_object)
-        session.add(analyzed_data)
-        session.refresh(analyzed_data)
-        analyzed_metadata = AnalyzedMetadata(analyzed_object, analyzed_data)
-        session.add(analyzed_metadata)
-        session.commit()
+        try:
+            analyzed_data = AnalyzedData(analyzed_object)
+            session.add(analyzed_data)
+            session.flush()
+            session.refresh(analyzed_data)
+            analyzed_metadata = AnalyzedMetadata(analyzed_object, analyzed_data)
+            session.add(analyzed_metadata)
+            if "auditID" in analyzed_object.original_object:
+                auditID = analyzed_object.original_object['auditID']
+                print(f"Audit with id {auditID} added")
+            session.commit()
+        except SQLAlchemyError as e:
+            print(f"Error occured: {e}")
+            session.rollback()
 
     return {
         'severity': analyzed_object.severity, 
